76190f5faeb50c09d5b73f84935c1440f4b8bb4f
Aaron Szerencses <as@imko.de>
2016-04-27 11:54:53 +0200

    Not sure about this. Might be unneccesary...

---
+++ partkeepr/config.php
@@ -0,0 +1,11 @@
+<?php
+namespace PartKeepr;
+use PartKeepr\Util\Configuration;
+
+Configuration::setOption("partkeepr.database.dbname", "partkeepr");
+Configuration::setOption("partkeepr.database.username", "partkeepr");
+Configuration::setOption("partkeepr.database.password", "partkeepr");
+Configuration::setOption("partkeepr.database.host", "mysql");
+Configuration::setOption("partkeepr.database.driver", "pdo_mysql");
+Configuration::setOption(" ? ", " ? ");
+?>
